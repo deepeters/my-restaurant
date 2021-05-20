4a85fadee4d6f507f5e5f6abdb1212f039cdcbe8
Dennis Peters <dennis95peters@gmail.com>
2021-05-20 18:29:30 +0300

    THis commit builds the first even for the button

--- app/src/main/java/com/example/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/myrestaurants/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.myrestaurants;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mfindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mfindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
+            mfindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                }
+            });
     }
 }
