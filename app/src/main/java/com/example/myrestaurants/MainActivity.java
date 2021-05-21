965f95ad905a6677a8e9f398cc8e625cbd2c1077
Dennis Peters <dennis95peters@gmail.com>
2021-05-21 08:49:53 +0300

    This commit gathers data from EditText

--- app/src/main/java/com/example/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/myrestaurants/MainActivity.java
@@ -4,24 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mfindRestaurantsButton;
+    private EditText mlocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mlocationEditText = (EditText) findViewById(R.id.locationEditText);
         mfindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
             mfindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     //Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                    String location = mlocationEditText.getText().toString();
+                    Log.d(TAG, location);
                     Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
                     startActivity(intent);
                 }
